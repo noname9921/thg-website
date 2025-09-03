23c35e13ad6e0cf01004f71cc1dac89c59580a64
noname9921 <baobao1102@gmail.com>
2025-09-03 10:27:46 +0700

    change backend

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const dotenv = require('dotenv')
-const __dirname = path.resolve();
 
 dotenv.config()
 app.use(express.static(path.join(__dirname, "public"), {
